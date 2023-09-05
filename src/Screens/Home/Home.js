e0d530899eabe0585a3006883ec0f553062fa45a
sairam <sairam7367@gmail.com>
2023-09-05 19:22:01 +0100

    home page search

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -11,20 +11,21 @@ const cardsToShow = 5;
 
 export default function Home() {
   const [startIndex, setStartIndex] = useState(0);
+  const [searchQuery, setSearchQuery] = useState('');
   const [cardData, setCardData] = useState([]);
 
   useEffect(() => {
-    // Fetch card data from the API
+
     axios
       .get(`${URL}/reviews/getreview`)
       .then((response) => {
-        const fetchedCardData = response.data.data; // Assuming your API response contains 'data' field
+        const fetchedCardData = response.data.data; 
         setCardData(fetchedCardData);
       })
       .catch((error) => {
         console.error('Error fetching card data:', error);
       });
-  }, []); // Empty dependency array to fetch data only once when the component mounts
+  }, []);
 
   const goToPrevious = () => {
     if (startIndex > 0) {
@@ -33,44 +34,60 @@ export default function Home() {
   };
 
   const goToNext = () => {
-    if (startIndex + cardsToShow < cardData.length) {
+    if (startIndex + cardsToShow < filteredCardData.length) {
       setStartIndex(startIndex + 1);
     }
   };
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    setStartIndex(0);
+  };
+
+  const filteredCardData = cardData.filter((card) =>
+    card.location.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <div className="home-container">
       <Header />
       <img className="header-image" src={home1} alt="" />
-      <div>
+      <div className='explore'>
         <h2>Explore</h2>
+      </div>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Location"
+          value={searchQuery}
+          onChange={(e) => handleSearch(e.target.value)}
+        />
       </div>
       <div className="card-carousel">
         <button className="nav-button" onClick={goToPrevious} disabled={startIndex === 0}>
           &lt;
         </button>
         <div className="card-container">
-          {cardData.slice(startIndex, startIndex + cardsToShow).map((card, index) => (
+          {filteredCardData.slice(startIndex, startIndex + cardsToShow).map((card, index) => (
             <div key={index} className="card">
               {/* Display the image with the determined format */}
-              <img
-                src={card.picture1}
-                alt={`Card ${index + startIndex}`}
-                className="card-image" // Set a class for styling
-              />
+              <img src={card.picture1} alt={`Card ${index + startIndex}`} className="card-image" />
+
               <h5>
                 <Link to={`/review/${card.id}`} style={{ color: '#f1b452' }}>
                   {card.title}
                 </Link>
               </h5>
-              <div>{card.location}</div>
+              <div>
+                {card.location}
+              </div>
             </div>
           ))}
         </div>
         <button
           className="nav-button"
           onClick={goToNext}
-          disabled={startIndex + cardsToShow >= cardData.length}
+          disabled={startIndex + cardsToShow >= filteredCardData.length}
         >
           &gt;
         </button>
